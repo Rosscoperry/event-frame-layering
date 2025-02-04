d96ed7b792fab84462dd8f389793e208e4636c97
Ross Perry <rosscoperryy@gmail.com>
2025-02-04 15:44:43 +0000

    ✨ add visualization functionality for event-based data and create video output

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 import pandas as pd
 import numpy as np
-from tonic import Dataset, transforms
-import torch
 import logging
 logger = logging.getLogger(__name__)
 logging.basicConfig(
@@ -12,6 +10,10 @@ logging.basicConfig(
         logging.StreamHandler()
     ]
 )
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+import matplotlib.animation as animation
+
 
 def merge_event_csv(data_csv1, data_csv2, output_file):
     """
@@ -53,8 +55,15 @@ def merge_event_csv(data_csv1, data_csv2, output_file):
     data_csv.sort_values(by=['t'], inplace=True)
 
     data_csv.to_csv(output_file, index=False)
+
+##### FINALISE COLOUR ARG ########
+def visualise_eventbased_data(path_to_csv_data, num_channels = 2, accumulation_time_us=33333, frame_rate = 30, color=False):
     
-def visualise_eventbased_data(path_to_csv_data, num_channels = 2, accumulation_time_us=33333, frame_rate = 30):
+    """
+    Visualise eventbased data from a csv file.
+    
+    
+    """
     # load in data 
     csv_data = pd.read_csv(path_to_csv_data)
     
@@ -80,20 +89,68 @@ def visualise_eventbased_data(path_to_csv_data, num_channels = 2, accumulation_t
     # create bin of time values with start and end times of each frame with delta_t
     time_bins = np.arange(csv_data['t'].iloc[0], csv_data['t'].iloc[-1], delta_t)
     
-    print(time_bins)
-    return 
+    # subtract accumulation times from each bin to get the start time of each frame
+    time_bins_start = time_bins - accumulation_time_us
+    time_bins_end = time_bins
+    
+    # create a list of frames with each frame containing the events that occur within the time bin into a numpy array
+    frames = np.empty((len(time_bins_start), num_channels), dtype=object)
+    for i in range(len(time_bins_start)):
+        frame = csv_data[(csv_data['t'] >= time_bins_start[i]) & (csv_data['t'] < time_bins_end[i])]
+        for channel in range(num_channels):
+            frames[i, channel] = frame[frame['c'] == channel].to_numpy()
+        
+    # frames shape (number of frames, number of channels)
+    logging.info("Frames shape: {}".format(frames.shape))
+    
+    # accumulate all the events in each frame with one colour for each channel
+    res = (720, 1280, 3)
 
+    # create a blank image append to video
+    video = []
     
+    # get colour list to represent value of pixel
+    colours = [[255, 0, 0], [0, 255, 0]]
+
+    for frame in frames:
+        img = np.zeros(res, dtype=np.uint8)
+        for channel in range(num_channels):
+            for event in frame[channel]:
+                rgb = colours[channel]
+                img[event[1], event[0]] = rgb if event[2] == 1 else [0,0,0] # Check if event is on or off      
+              
+        video.append(img) 
+        
+      
+    # combine list of numpy.ndarrays into a video saved to a file 
+    logging.info("Creating video from frames")
+    fig = plt.figure()
+    ims = []
+    for i in range(len(video)):
+        im = plt.imshow(video[i], animated=True)
+        ims.append([im])
+    ani = animation.ArtistAnimation(fig, ims, interval=1000/frame_rate, blit=True, repeat_delay=1000) #interval in ms 
+    ani.save('video.mp4')
+    logging.info("Video saved as video.mp4")
     
+    return video
+
+def split_colours(number_of_colours):
+    """
+    creates an array of rgb colours to use in visualisation as specified by user
+    """
     
+    colours = []
+    for i in range(number_of_colours):
+        colours.append([np.random.randint(0, 255), np.random.randint(0, 255), 0])
+    
+    return colours
 
 
 merge_event_csv("East_5s.csv", "West_5s.csv", "merged.csv")
 
-visualise_eventbased_data("merged.csv", 2, accumulation_time_us=1000000, frame_rate=30)
+visualise_eventbased_data("merged.csv", 2, accumulation_time_us=33333, frame_rate=30)
 
-
-# function to visualize the data 
 
 # function that begins pipeline towards training an snn
 
